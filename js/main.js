6743f7ad88e71bfbfb90af135e2e89ac18bd8be6
Andrii Yakymyshyn <andrewyakymyshyn@gmail.com>
2020-04-14 15:49:58 +0300

    fixed issues in second section

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let logo = document.querySelector('.main-logo');
 
     mobileMenuTrigger.addEventListener('click', () => {
-        mobileMenuOverlay.classList.add('visible');
-        closeIcon.classList.add('db');
-        logo.classList.add('white');
+        addClass(mobileMenuOverlay, 'visible');
+        addClass(closeIcon, 'db');
+        addClass(logo, 'white');
     });
     closeIcon.addEventListener('click', () => {
         mobileMenuOverlay.classList.remove('visible');
@@ -16,12 +16,34 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     window.addEventListener('scroll', () => {
         let secondSection = document.getElementById('second-section');
-        if (window.pageYOffset > 300) {
-            secondSection.classList.add('black-section');
+        let tags = document.querySelectorAll('ul.ss-tags li');
+        let underline = document.querySelector('.full-width-underline');
+        let heading = document.querySelector('.ss-heading');
+        let itemTitle = document.querySelectorAll('.item-title');
+        let itemUnderline = document.querySelectorAll('.underline');
+
+        if (window.pageYOffset > 480) {
+            addClass(secondSection, 'black-section');
+            addClass(underline, 'pink-background');
+            addClass(heading, 'color-pink');
+            itemUnderline.forEach(underline => addClass(underline, 'pink-background'));
+            itemTitle.forEach(title => addClass(title, 'color-pink'));
+            tags.forEach(tag => addClass(tag, 'color-pink'));
         }
         else {
-            if (secondSection.classList.contains('black-section'))
-                secondSection.classList.remove('black-section');
+            removeClass(secondSection, 'black-section');
+            removeClass(underline, 'pink-background');
+            removeClass(heading, 'color-pink');
+            itemUnderline.forEach(underline => removeClass(underline, 'pink-background'));
+            tags.forEach(tag => removeClass(tag, 'color-pink'));
+            itemTitle.forEach(title => removeClass(title, 'color-pink'));
         }
     });
 });
+
+const addClass = (element, className) => {
+    element.classList.add(className);
+};
+const removeClass = (element, className) => {
+    element.classList.contains(className) && element.classList.remove(className);
+};
